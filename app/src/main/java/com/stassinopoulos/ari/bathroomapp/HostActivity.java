1b056a3f78308ab4de57f4aef6a5b138a40d39ef
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2019-03-05 09:19:25 -0800

    Add tab switcher

--- app/src/main/java/com/stassinopoulos/ari/bathroomapp/HostActivity.java
+++ app/src/main/java/com/stassinopoulos/ari/bathroomapp/HostActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.RelativeLayout;
@@ -12,8 +13,9 @@ import android.widget.RelativeLayout;
 public class HostActivity extends AppCompatActivity {
 
     private TabLayout mTabLayout;
-    private Toolbar mToolbar;
+    //private Toolbar mToolbar;
     private RelativeLayout mRootView;
+    private HostTabType mHostTabType;
 
     public enum HostTabType {
         HOME_TAB,
@@ -27,33 +29,23 @@ public class HostActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_host);
         mTabLayout = findViewById(R.id.host_tab_layout);
-        mToolbar = findViewById(R.id.host_toolbar);
         mRootView = findViewById(R.id.host_relative_layout);
 
         mTabLayout.addTab(mTabLayout.newTab().setText("Home").setTag(HostTabType.HOME_TAB));
         mTabLayout.addTab(mTabLayout.newTab().setText("Report").setTag(HostTabType.REPORT_TAB));
         mTabLayout.addTab(mTabLayout.newTab().setText("Request").setTag(HostTabType.REQUEST_TAB));
 
-        mToolbar.setTitle("Home");
+        getSupportActionBar().setElevation(0.0f);
+
+        mTabLayout.setTabTextColors(ContextCompat.getColor(this, R.color.colorText), ContextCompat.getColor(this, R.color.colorText));
+
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
                 if(!(tab.getTag() instanceof HostTabType))
                     return;
 
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                switch((HostTabType) tab.getTag()) {
-
-                    case HOME_TAB:
-                        ft.replace(R.id.host_relative_layout, new HomeFragment());
-                        break;
-                    case REPORT_TAB:
-                        ft.replace(R.id.host_relative_layout, new ReportFragment());
-                        break;
-                    case REQUEST_TAB:
-                        ft.replace(R.id.host_relative_layout, new RequestFragment());
-                        break;
-                }
+                switchTab((HostTabType) tab.getTag());
             }
 
             @Override
@@ -66,6 +58,30 @@ public class HostActivity extends AppCompatActivity {
 
             }
         });
+
+        switchTab(HostTabType.HOME_TAB);
+    }
+
+    private void switchTab(HostTabType hostTabType) {
+        if(mHostTabType != hostTabType) {
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+
+            switch(hostTabType) {
+                case HOME_TAB:
+                    ft.replace(R.id.host_relative_layout, new HomeFragment());
+                    break;
+                case REPORT_TAB:
+                    ft.replace(R.id.host_relative_layout, new ReportFragment());
+                    break;
+                case REQUEST_TAB:
+                    ft.replace(R.id.host_relative_layout, new RequestFragment());
+                    break;
+            }
+
+            ft.commit();
+
+            mHostTabType = hostTabType;
+        }
     }
 
 
